dab74cebe341768cf524b9a4ba963ad74068de80
Russell Bayley <russell.bayley@gmail.com>
2021-01-04 09:45:27 +1100

    Update clear_display.py

--- clear_display.py
+++ clear_display.py
@@ -5,18 +5,18 @@ from os import path
 
 
 # Create ST7735 LCD display class. If using ST7789, delete the st7735 coding. then uncomment the ST7789
-    disp = ST7735.ST7735(
-        port=0,
-        cs=0,   #ST7735.BG_SPI_CS_FRONT,  # BG_SPI_CSB_BACK or BG_SPI_CS_FRONT
-        dc=9,
-        backlight=13,               
-        rst=22,
-        width=128,
-        height=160,
-        rotation=270,
-        invert=False,
-        spi_speed_hz=4000000
-    )
+disp = ST7735.ST7735(
+    port=0,
+    cs=0,   #ST7735.BG_SPI_CS_FRONT,  # BG_SPI_CSB_BACK or BG_SPI_CS_FRONT
+    dc=9,
+    backlight=13,               
+    rst=22,
+    width=128,
+    height=160,
+    rotation=270,
+    invert=False,
+    spi_speed_hz=4000000
+)
 
 
 disp.begin()
